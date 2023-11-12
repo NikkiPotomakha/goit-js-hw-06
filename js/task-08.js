2c57868532070bf2a5408dca298e13c739c25231
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-12 17:25:47 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailValue = this.elements.email.value;
+  const passwordValue = this.elements.password.value;
+
+  if (!emailValue || !passwordValue) {
+    alert("All fields must be filled!");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  this.reset();
+});
