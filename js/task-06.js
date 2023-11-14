3197ea7a170d3f29d6335c2fdcb064d78e9763c0
NikkiPotomakha <serhiienkoveronika@gmail.com>
2023-11-14 15:35:58 +0100

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,15 @@
-const validationInput = document.getElementById("#validation-input");
+const validationInput = document.querySelector("#validation-input");
 
-validationInput.addEventListener("blur", () => {
-  const inputValue = validationInput.value.trim();
-  const expectedLength = parseInt(validationInput.getAttribute("data-length"));
+function verification(event) {
+  const inputValueLength = Number(validationInput.value.trim().length);
+  const inputDataLength = Number(validationInput.dataset.length);
 
-  if (inputValue.length === expectedLength) {
+  if (inputValueLength === inputDataLength) {
     validationInput.classList.add("valid");
     validationInput.classList.remove("invalid");
   } else {
     validationInput.classList.add("invalid");
     validationInput.classList.remove("valid");
   }
-});
+}
+validationInput.addEventListener("blur", verification);
